8672c4720bb5689b9f0af47452227dc40d08d11c
hatmabrouk <68492700+hatmabrouk@users.noreply.github.com>
2020-09-03 14:01:19 -0500

    Create Part 1.sol

---
+++ Part 1.sol
@@ -0,0 +1,19 @@
+pragma solidity 0.6.6;
+
+contract HelloWorld {
+    
+string public message = "Hello World";
+
+uint public number = 123;
+
+bool public ishappy = true;
+
+address public contractCreator = 0xBf2916234279507F56f8A4F79889529dd9CCA018;
+
+uint [] public numbers = [1,20,45];
+//This array here does not need a function, but there is the getNumber function to see how the function works.
+
+string [] public messages = ["hello", "hello hatem", "hello world"];
+// This array does not need a function to be displayed.
+
+}
