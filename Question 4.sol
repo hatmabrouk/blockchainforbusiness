362782ad22dcb3b64910924ef1ce891a01c07a00
hatmabrouk <68492700+hatmabrouk@users.noreply.github.com>
2020-09-04 03:36:33 -0500

    Update Question 4.sol

--- Question 4.sol
+++ Question 4.sol
@@ -11,10 +11,8 @@ bool public ishappy = true;
 address public contractCreator = 0xBf2916234279507F56f8A4F79889529dd9CCA018;
 
 uint [] public numbers = [1,20,45];
-//This array here does not need a function, but there is the getNumber function to see how the function works.
 
 string [] public messages = ["hello", "hello hatem", "hello world"];
-// This array does not need a function to be displayed.
 
 function getMessage () public view returns (string memory){
 return message;
