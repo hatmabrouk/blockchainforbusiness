045ce2fa278348f3497dfa7faca3245ef4d4d2d7
hatmabrouk <68492700+hatmabrouk@users.noreply.github.com>
2020-09-04 03:42:16 -0500

    Update Question 4.sol

--- Question 4.sol
+++ Question 4.sol
@@ -12,7 +12,7 @@ address public contractCreator = 0xBf2916234279507F56f8A4F79889529dd9CCA018;
 
 uint [] public numbers = [1,20,45];
 
-string [] public messages = ["hello", "hello hatem", "hello world"];
+string [] public messages = ["hello", "hello Karla", "hello world"];
 
 function getMessage () public view returns (string memory){
 return message;
