bbd2fbece33bb384bb6398e6eecca96aff68d14c
hatmabrouk <68492700+hatmabrouk@users.noreply.github.com>
2020-10-08 18:37:36 -0500

    Create Part 10: Modifiers.sol

---
+++ Part 10: Modifiers.sol
@@ -0,0 +1,80 @@
+pragma solidity 0.6.6;
+contract Identity {
+    struct Person{
+        string name;
+        uint age;
+        uint height;
+        bool senior;
+    }
+    address public owner;
+    
+    modifier onlyOwner (){
+        //the modifier name is onlyOwner because we want to restrict some functions to use of the owner only.
+        require (msg.sender == owner); 
+        // here we just moved the require function to here instead of under the delete and getCreator functions. 
+        _; 
+        //this means continue the execution. 
+    }
+    
+    constructor () public{
+        owner = msg.sender;
+    }
+    
+    mapping (address =>Person) private people;
+    address [] private creators;
+    
+    function createPerson (string memory name, uint age, uint height) public{
+        require (age<150, "age must be below 150");
+        Person memory newPerson;
+        newPerson.name = name;
+        newPerson.age = age;
+        newPerson.height = height;
+        if(age >=65) {
+            newPerson.senior = true;
+        }
+        else {
+            newPerson.senior = false;
+        }
+        insertPerson (newPerson);
+        creators.push (msg.sender);
+        assert (
+            keccak256 (
+                abi.encodePacked (
+                    people[msg.sender].name, 
+                    people[msg.sender].age, 
+                    people[msg.sender].height, 
+                    people[msg.sender].senior
+                )
+            ) 
+            == 
+            keccak256 (
+                abi.encodePacked (
+                    newPerson.name, 
+                    newPerson.age, 
+                    newPerson.height, 
+                    newPerson.senior
+                )
+            )
+        );
+    }
+    
+    function insertPerson (Person memory newPerson) private{
+        address creator = msg.sender;
+        people[creator] = newPerson;
+    }
+    
+    function getPerson () public view returns (string memory name, uint age, uint height, bool senior){
+        address creator = msg.sender;
+        return (people[creator].name, people[creator].age, people[creator].height, people[creator].senior);
+    }
+    
+    function deletePerson (address creator) public onlyOwner {
+        delete people[creator];
+        assert (people[creator].age == 0);
+    }
+    
+    function getCreator (uint index) public view onlyOwner returns (address){
+        return creators[index];
+    }
+    
+}
