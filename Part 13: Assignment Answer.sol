3188b6723ec7fecef6eb373521ca394ec18ece88
Hatem Mabrouk <68492700+hatmabrouk@users.noreply.github.com>
2020-10-29 15:04:04 -0600

    Update Part 13: Assignment Answer.sol

--- Part 13: Assignment Answer.sol
+++ Part 13: Assignment Answer.sol
@@ -15,11 +15,17 @@ contract MemoryAndStorage {
     function updateBalance(uint id, uint balance) public {
      users[id].balance = balance;
     }
-    // You are updating the balance here based on the id
+        // a user (with small u) is a new information. It is defined by referring to the User struct saved in storage. 
+        // Thus, it consists of an id and a balance. Thus user id and balance must be explained. 
+        // This user is going to have an id within the users mapping. so no changes to the id.
+        // The id within the struct and the users mapping, is getting stored within the User memory variable.
+        
+        // what about the balance?
         
         // The problem we have here is that we tried to store the id of the mapping users
         // within a user memory variable. And then, we went and modified that user memory variable 
-        // and changed its balance. Consequently, user became a local variable stored in memory. 
+        // and changed its balance, as explained below. Consequently, user became a local variable stored in memory. 
+        
         //The easiest solution is to change memory to storage. This will allow the local variable user 
         // to be a direct pointer to the user that is in the mapping 
         //because user then will refer to the mapping value in storage.
